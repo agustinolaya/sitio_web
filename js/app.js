f714b25e6a41b67bedd8c76eedea613862f471ff
Agustin <agustinolaya@gmail.com>
2017-01-18 17:06:27 -0700

    CSS Finalizado Inicio JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 // TODO agregar código javascript
+// alert('Hola Mundo');
+console.log('Hola desde la consola de javascript juejnujeueujeue xd');
+
+function login(){
+    var usuario = document.getElementById('txtUsuario').value;
+    var passwd = document.getElementById('txtPassword').value;
+    alert('Usuario: '+ usuario + ', Contraseña: ' + passwd);
+}
